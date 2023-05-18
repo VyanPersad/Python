d8b3ec1e52a97f96d509c2cf288a17e17f3e384e
Vyan <vyanpersad@gmail.com>
2023-05-17 21:43:40 -0400

    Updated and completed webmaps.
    Finished webmaps with python and folium.

--- Webmaps/webmaps.py
+++ Webmaps/webmaps.py
@@ -6,7 +6,13 @@ import pandas
 #For zoom a lower number zoom out a higher number zooms in.
 map = folium.Map(location=[38.58, -99.09], zoom_start=7)
 
+#You can have many different groups for diferent things.
+#Remember for each child to attach the apppropriate feature group
+#It allows for each feature to be its own layer, good for consistency
 feature = folium.FeatureGroup(name="MyMap")
+f_vol = folium.FeatureGroup(name="Volcanoes")
+f_pop = folium.FeatureGroup(name="Population")
+
 feature.add_child(
     folium.Marker(location=[38.2, -99.1],
                   popup="Hi I'm here!!",
@@ -15,6 +21,7 @@ feature.add_child(
     folium.Marker(location=[37.2, -97.1],
                   popup="Hi I'm here!!",
                   icon=folium.Icon(color='blue')))
+
 #A better way would be to use a for loop
 #Essentially the for loop iterates through the list of co-ords
 #plotting them as per the code
@@ -41,7 +48,7 @@ def colour_with_elev(elevation):
 
 
 for la, ln, ele in zip(lat, lon, elev):
-    feature.add_child(
+    f_vol.add_child(
         folium.CircleMarker(
             location=[la, ln],
             popup=folium.Popup(str(ele) + " m", parse_html=True),
@@ -52,11 +59,23 @@ for la, ln, ele in zip(lat, lon, elev):
             radius=6,
         ))
 
-feature.add_child(
+#The values in the [] brackets are from the dataset columns
+#In this case the lambda x :{} is a type of functon that can be inserted
+#inline to add function to style code so that is can change with data points.
+f_pop.add_child(
     folium.GeoJson(
-        data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+        data=open('world.json', 'r', encoding='utf-8-sig').read(),
+        style_function=lambda x: {
+            'fillColor':
+            'green' if x['properties']['POP2005'] < 10000000 else 'orange'
+            if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'
+        }))
 
 #Helps to beter organise features and code.
 map.add_child(feature)
+map.add_child(f_vol)
+map.add_child(f_pop)
+#Ensure that this is added after you add all the other children and feature groups
+map.add_child(folium.LayerControl())
 #Saves the map to a html file
 map.save("Map.html")
