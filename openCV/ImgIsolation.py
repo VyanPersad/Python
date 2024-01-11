4bf1dea1abbe8930cc8235ed17c01ba628ebb348
VyanPersad <vyanpersad@gmail.com>
2024-01-10 21:32:01 -0400

    Add files via upload

--- openCV/ImgIsolation.py
+++ openCV/ImgIsolation.py
@@ -1,13 +1,19 @@
 import cv2
 import numpy as np
+from PIL import Image
 
 #0<--Blk+++White-->255
 
-filename = 'bkl2.png'
+filename = 'bkl4.png'
 img = cv2.imread(filename)
 gryImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-threshold = 125
-maxVal = threshold+50
+
+max_pix = np.amax(gryImg.astype(np.int32))
+min_pix = np.amin(gryImg.astype(np.int32))
+threshold = (max_pix+min_pix)/2
+
+maxVal = threshold+25
+print(max_pix,min_pix,threshold,maxVal)
 #For threshold the vales above are considered.
 #Threshold outputs 2 values the first is the threshold the other is the image.
 _, msk = cv2.threshold(gryImg, threshold, maxVal, cv2.THRESH_BINARY_INV)
@@ -16,7 +22,7 @@ contours, hierarchy = cv2.findContours(msk, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_
 for c in contours:
     # Get the coordinates and dimensions of the bounding rectangle
     x, y, w, h = cv2.boundingRect(c)
-    # Draw the rectangle on the original image with a color of your choice and a thickness of 2 pixels
+    #Draw the rectangle on the original image with a color of your choice and a thickness of 2 pixels
     cv2.rectangle(gryImg, (x, y), (x + w, y + h), (0, 255, 0), 2)
     #cv2.drawContours(gryImg, [c], 0, (255, 0, 255), 2)
 
