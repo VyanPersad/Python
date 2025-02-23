866814860463b371253dbef0ebef6c355abc97ed
VyanPersad <vyanpersad@gmail.com>
2025-02-23 00:37:37 -0400

    WebScraper work
    It works for some websites.
    other websites block scraping so still working on that.

--- webScraping/webScraperSelenium.py
+++ webScraping/webScraperSelenium.py
@@ -5,16 +5,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
 import time
 
-website1 = 'https://www.pricesmart.com/en-tt/category/Electronics/E10D24?fq=category%3B%22E10D24015%22'
-website2 = 'https://www.standardtt.com/search?type=product%2Cpage%2Carticle&options%5Bprefix%5D=last&q=Samsung'
+website1 = 'https://www.pricesmart.com/en-tt/category/Electronics/E10D24?fq=category%3B"E10D24015"'
+website2 = 'https://www.standardtt.com/search?q=Samsung&type=article%2Cpage%2Cproduct&options%5Bprefix%5D=last&sort_by=relevance&filter.p.product_type=ELECTRONICS&filter.v.availability=1&filter.p.m.custom.brand=SAMSUNG'
 webDriver_path = "C:\webdrivers\msedgedriver.exe"
 options = webdriver.EdgeOptions()
 driver = webdriver.Edge(options=options)
-driver.get(website1)
-time.sleep(1)
+driver.get(website2)
+time.sleep(30)
 
-prices = driver.find_elements(By.XPATH,'//*[@id="product-card-485340"]/div[2]/div[2]/div[1]/div[1]/div/div/del')
-time.sleep(4)
+prices = driver.find_elements(By.CLASS_NAME,"price__current")
+time.sleep(30)
 print(len(prices))
+
 for price in prices:
     print(price.text)
+
