8d9aa15a20f62723f214d02803054db8f6dcd4c5
VyanPersad <vyanpersad@gmail.com>
2025-08-03 02:07:58 -0400

    pdf File Reader
    Allowed for reading pdf file and dumping text to .csv.

--- pdfReader/readPdf.py
+++ pdfReader/readPdf.py
@@ -1,27 +1,48 @@
-import pdfplumber
+import pdfplumber, os
 
-pdf_path =r"C:\Users\Vyan\Downloads\PDF_296429_68297599.pdf"
+pdf_path =r"C:\Users\Vyan\Downloads\AUG_2025_TCL"
+dest_file = open('test.csv', 'a')
+all_tables = []
+cleaned_tables = []
+for file in os.listdir(pdf_path):
 
-with pdfplumber.open(pdf_path) as pdf:
-    all_tables=[]
+    with pdfplumber.open(os.path.join(pdf_path, file)) as pdf:
 
-    for page in pdf.pages:
-        #text = page.extract_text()
-        #print(text)
-        tables = page.extract_tables()
+        table_settings = {
+            "vertical_strategy":"text",
+            "horizontal_strategy":"text",
+            "text_tolerance":10,
+            "min_words_horizontal":1,
+        }
+        pg = pdf.pages[0]
+
+        tables = pg.extract_tables(table_settings=table_settings)
+        [2,3,4,6,9]
+        #print(tables) 
         for table in tables:
-            print(table)
-
-    table_settings = {
-        "vertical_strategy":"text",
-        "horizontal_strategy":"text",
-        "text_tolerance":10,
-        "min_words_horizontal":3,
-    }
-    pg = pdf.pages[0]
-
-    tables = pg.extract_tables(table_settings=table_settings)
-    [2,3,4,6,9]
-    print(tables[0][15]) 
+            #print(table)
+            all_tables.append(table)
  
+'''
+for table in all_tables:
+    for row in table:
+        for i in range(len(row)):
+            for item in row:
+                if item == '' or item is None or item == ' ':
+                    row.remove(item)
+        if row is not None:
+            print(row)
+'''
+for table in all_tables:    
+    for row in table:
+        cleaned_row=[]
+        for item in row:
+            if item != '':
+                cleaned_row.append(item)
+        if cleaned_row != []:
+            cleaned_tables.append(cleaned_row)        
+    
+for row in cleaned_tables:
+    dest_file.write(f"{",".join(row)}\n")
 
+dest_file.close()
