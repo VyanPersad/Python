58bc3fe8fa9d5212e46eaa5eddefddfb7fa63f0a
VyanPersad <vyanpersad@gmail.com>
2025-08-16 18:16:41 -0400

    Added better column detection.
    Used the explicit table settings to better define the columns.

--- pdfReader/readPdf.py
+++ pdfReader/readPdf.py
@@ -1,22 +1,25 @@
 import pdfplumber, os
 
-pdf_path =r"C:\Users\Vyan\Downloads\AUG_2025_TCL"
-dest_file = open('test.csv', 'a')
+pdf_path =r"C:\Users\Vyan\Downloads\PDF_297161_68367377.pdf"
+#dest_file = open('test.csv', 'a')
 all_tables = []
 cleaned_tables = []
-for file in os.listdir(pdf_path):
+bounding_box = (0,210,612,625)
+#for file in os.listdir(pdf_path):
 
-    with pdfplumber.open(os.path.join(pdf_path, file)) as pdf:
+with pdfplumber.open(pdf_path) as pdf:
 
         table_settings = {
-            "vertical_strategy":"text",
+            "vertical_strategy":"explicit",
+            "explicit_vertical_lines":[0, 45, 330, 465, 545, 612],
             "horizontal_strategy":"text",
-            "text_tolerance":10,
+            "text_tolerance":2,
             "min_words_horizontal":1,
         }
         pg = pdf.pages[0]
-
-        tables = pg.extract_tables(table_settings=table_settings)
+        pg_crp = pg.crop(bounding_box)
+        pg_crp.to_image().debug_tablefinder(table_settings=table_settings).show()
+        tables = pg_crp.extract_tables(table_settings=table_settings)
         [2,3,4,6,9]
         #print(tables) 
         for table in tables:
@@ -41,8 +44,9 @@ for table in all_tables:
                 cleaned_row.append(item)
         if cleaned_row != []:
             cleaned_tables.append(cleaned_row)        
-    
-for row in cleaned_tables:
-    dest_file.write(f"{",".join(row)}\n")
 
-dest_file.close()
+print(cleaned_tables)    
+#for row in cleaned_tables:
+#   dest_file.write(f"{",".join(row)}\n")
+
+#dest_file.close()
