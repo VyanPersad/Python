36d0d5bb6f180f71816769665c2e1256ad9d2d37
VyanPersad <vyanpersad@gmail.com>
2025-08-17 03:31:26 -0400

    Cleaned up
    Cleaned up the output array and tables.

--- pdfReader/readPdf.py
+++ pdfReader/readPdf.py
@@ -1,9 +1,10 @@
-import pdfplumber, os
+import pdfplumber, os, re
 
 pdf_path =r"C:\Users\Vyan\Downloads\PDF_297161_68367377.pdf"
-#dest_file = open('test.csv', 'a')
+dest_file = open('test.csv', 'a')
 all_tables = []
 cleaned_tables = []
+calc_tables = []
 bounding_box = (0,210,612,625)
 #for file in os.listdir(pdf_path):
 
@@ -18,35 +19,35 @@ with pdfplumber.open(pdf_path) as pdf:
         }
         pg = pdf.pages[0]
         pg_crp = pg.crop(bounding_box)
-        pg_crp.to_image().debug_tablefinder(table_settings=table_settings).show()
-        tables = pg_crp.extract_tables(table_settings=table_settings)
-        [2,3,4,6,9]
-        #print(tables) 
-        for table in tables:
-            #print(table)
-            all_tables.append(table)
- 
-'''
-for table in all_tables:
-    for row in table:
-        for i in range(len(row)):
-            for item in row:
-                if item == '' or item is None or item == ' ':
-                    row.remove(item)
-        if row is not None:
-            print(row)
-'''
+        #pg_crp.to_image().debug_tablefinder(table_settings=table_settings).show()
+        all_tables = pg_crp.extract_tables(table_settings=table_settings)
+
 for table in all_tables:    
     for row in table:
         cleaned_row=[]
+        cleaned_item = ''
         for item in row:
-            if item != '':
-                cleaned_row.append(item)
-        if cleaned_row != []:
+            if item is not None:
+                cleaned_item = re.sub(r'\(cid:\d+\)','',item).strip() 
+                cleaned_item = cleaned_item.replace('US$','')  
+            #if "/" in cleaned_item:
+            #   cleaned_item = cleaned_item.split('/')
+            if cleaned_item != '':
+                cleaned_row.append(cleaned_item) 
+        if cleaned_row != [] and len(cleaned_row)>1:
+            cleaned_row[0:1] = cleaned_row[0].split(' ', maxsplit=1)
+            cleaned_row[2:3] = cleaned_row[2].split('/', maxsplit=1)
+            cleaned_row[3:4] = cleaned_row[3].split(' ', maxsplit=1)
+            cleaned_row[5:6] = cleaned_row[5].split(' ', maxsplit=1)
+            for item in cleaned_row:
+                if item == '': 
+                    cleaned_row.remove(item)
             cleaned_tables.append(cleaned_row)        
 
-print(cleaned_tables)    
-#for row in cleaned_tables:
-#   dest_file.write(f"{",".join(row)}\n")
+for row in cleaned_tables:
+    print(row)
 
-#dest_file.close()
+for row in cleaned_tables:
+   dest_file.write(f"{",".join(row)}\n")
+
+dest_file.close()
