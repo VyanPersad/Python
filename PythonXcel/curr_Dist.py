f94b7997f40404be6a3dcf8c97737e7553aaa1be
VyanPersad <vyanpersad@gmail.com>
2026-04-04 03:16:55 -0400

    Added another script to work with incoming items.

--- PythonXcel/curr_Dist.py
+++ PythonXcel/curr_Dist.py
@@ -2,8 +2,6 @@ from readFunction import read_from_file
 import pandas as pd
 from xlsxwriter import Workbook
 import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
 import datetime as dt
 
 curr_date = dt.datetime.now().strftime('%d.%m.%Y')
@@ -12,41 +10,47 @@ file_path = r'C:\Users\Vyan\Documents\GitHub\Python\PythonXcel\Data\STOCK QUERY 
 
 df = read_from_file(filepath=file_path, test=0, sheet=5)
 
-#print(df)
 Loc_Name = df['LocationName'].unique()
  
 Loc_Name = df[
-    (df['LocationName'].str.startswith('O') == False)
-    &
     (df['SKU'].str.startswith('T') == False)
     &
     (df['SKU'].str.startswith('O') == False)
-]
+    ]
 
 table = Loc_Name.pivot_table(
-                       index='SKU', 
+                       index=['SKU','PosDescription'], 
                        columns='LocationName', 
                        values='StockOnHand', 
-                       aggfunc=sum,
+                       aggfunc='sum',
                        fill_value=0,
                        margins=True,
                        margins_name='Total'
                        )
 
-to_print = r'Data\Combined_Current_Distribution_{curr_date}.xlsx'
+locs =[tot for tot in table.columns if tot != 'Total']
+loc_rnk = table.iloc[:-1][locs].sum().sort_values(ascending=False).index
+table = table[list(loc_rnk) + ['Total']]
+
+index_count = len(table.index.names)
+print(index_count)
+
+to_print = fr'Data\Combined_Current_Distribution_{curr_date}.xlsx'
 with pd.ExcelWriter(to_print, engine='xlsxwriter') as writer:
-    table.to_excel(writer, sheet_name='CurrDist')
+    table.to_excel(writer, sheet_name='Summary')
 
     max_row = table.shape[0]
     max_col = table.shape[1]
 
     wrkbook = writer.book
-    worksheet = writer.sheets['CurrDist']
+    worksheet = writer.sheets['Summary']
     worksheet.autofilter(0, 0, max_row, max_col)
 
-    red_fill = wrkbook.add_format({
-        'bg_color': '#FFC7CE',
-        'font_color': '#9C0006'
+    gray_fill = wrkbook.add_format({
+        'bold': True,
+        'bg_color': "#BFBFBF",
+        'font_color': "#000000",
+        'align': 'center',
     })
 
     header_format = wrkbook.add_format({
@@ -57,14 +61,41 @@ with pd.ExcelWriter(to_print, engine='xlsxwriter') as writer:
         'border': 1
     })
 
+    index_format = wrkbook.add_format({
+        'bold': True,
+        'align': 'left',
+        'valign': 'vcenter',
+        'border': 1
+    })
+
+    col_format = wrkbook.add_format({
+        'bold': False, 
+        'align': 'left'
+    })
+
+    cell_format = wrkbook.add_format({
+        'align': 'center',
+        'valign': 'vcenter',
+        'border': 1
+    })
+
     cond_format = {
         'type':     'cell',
         'criteria': '>',
         'value':    1,
-        'format':   red_fill
+        'format': gray_fill
     }
 
-    for col_num, value in enumerate(table.columns.values):
-        worksheet.write(0, col_num + 1, value, header_format)
-        worksheet.conditional_format(1, col_num + 1, max_row, max_col, cond_format)
+    worksheet.set_column(0,0, 20, col_format)
+    worksheet.set_column(1,1, 70, col_format)
 
+    worksheet.write(0,0, 'SKU', index_format)
+    worksheet.write(0,1, 'PosDescription', index_format)   
+
+    for col_num, value in enumerate(table.columns.values):
+        worksheet.write(0, 0, value, index_format)
+        worksheet.write(0, 1, value, index_format)
+
+        worksheet.conditional_format(1, col_num + index_count, max_row, col_num + index_count, cond_format)
+
+print("File writing completed")
---
+++ PythonXcel/pending.py
@@ -0,0 +1,23 @@
+from readFunction import read_from_file
+import datetime as dt
+
+this_yr = dt.datetime.now().year
+print(this_yr)
+curr_mon = dt.datetime.now().month
+print(curr_mon)
+
+file_path = r'C:\Users\Vyan\Documents\GitHub\Python\PythonXcel\Data\Reportes Courts.xlsx'
+
+df = read_from_file(filepath=file_path, test=0)
+
+vision_Filter = df[df['Department'].str.contains('VISION', na=False)]
+audio_Filter = df[df['Department'].str.contains('AUDIO', na=False)]
+gaming_Filter = df[df['Department'].str.contains('GAMING', na=False)]
+
+grouped_df = vision_Filter[['Class', 'Brand', 'Code Courts', 'Model','Description','Expected Delivery Date','Pending']]
+
+date_df = grouped_df[
+    (grouped_df['Expected Delivery Date'].dt.year == this_yr) 
+    & 
+    (grouped_df['Expected Delivery Date'].dt.month == curr_mon)]
+
