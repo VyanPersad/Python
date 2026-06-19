f3b638a951281eaa22c0c67956356322747c7734
VyanPersad <vyanpersad@gmail.com>
2026-06-19 03:25:49 -0400

    Some minor tweeks.

--- PythonXcel/pending.py
+++ PythonXcel/pending.py
@@ -7,16 +7,9 @@ curr_mon = dt.datetime.now().month
 last_mon = curr_mon - 1
 next_mon = curr_mon + 1
 
+
 file_path = r'C:\Users\Vyan\Documents\GitHub\Python\PythonXcel\Data\Raw_Data\Raw_Data\Reportes Courts.xls'
-file_path_2 = r'C:\Users\Vyan\Documents\GitHub\Python\PythonXcel\Data\Arriving_Soon.xlsx'
-
 df = read_from_file(filepath=file_path, test=0)
-df_2 = read_from_file(filepath=file_path_2, test=0)
-
-vision_Filter = df[df['Department'].str.contains('VIDEO', na=False)]
-audio_Filter = df[df['Department'].str.contains('AUDIO', na=False)]
-gaming_Filter = df[df['Department'].str.contains('GAMING', na=False)]
-
 major_Filter = df[
                   (df['Department'].str.contains('VIDEO', na=False)) |
                   (df['Department'].str.contains('AUDIO', na=False)) |
@@ -28,17 +21,18 @@ grouped_df = major_Filter[['Class', 'Brand', 'Code Courts', 'Model','Description
 date_df = grouped_df[
     (grouped_df['Expected Delivery Date'].dt.year == this_yr) 
     & 
-    (grouped_df['Expected Delivery Date'].dt.month == curr_mon)
+    ((grouped_df['Expected Delivery Date'].dt.month == curr_mon)
     |
     (grouped_df['Expected Delivery Date'].dt.month == last_mon)
     |
-    (grouped_df['Expected Delivery Date'].dt.month == next_mon)
+    (grouped_df['Expected Delivery Date'].dt.month == next_mon))
     ]
+print(date_df.head(10))
 
-filter_df = df_2[['STATUS', 'RWT', 'ETA', 'CONTAINER#', 'SUPPLIER', 'DESCRIPTIONS']]
-
-date = pd.to_datetime(filter_df['ETA'], errors='coerce')
-curr_mon = dt.datetime.now().month
+file_path_2 = r'C:\Users\Vyan\Documents\GitHub\Python\PythonXcel\Data\Arriving_Soon.xlsx'
+df_2 = read_from_file(filepath=file_path_2, test=0)
+filter_df = df_2[['STATUS', 'RWT', 'ETA', 'CONTAINER#', 'SUPPLIER', 'DESCRIPTIONS']].copy()
+date = pd.to_datetime(filter_df['ETA'], format='%d-%b-%Y',errors='coerce')
 
 filter_df['ETA'] = date.dt.date
 filter_df['Month'] = date.dt.month.astype('Int64')
@@ -50,13 +44,15 @@ arrival_df = filter_df[
     (filter_df['Year'] >= 2026)
     &
     (filter_df['DESCRIPTIONS'].str.contains(search_terms, case=False, na=False))
-    #&
-    #(filter_df['Month'] >= curr_mon)
+    &
+    (filter_df['Month'] >= last_mon)
     ]
+
 arrival_df = arrival_df.sort_values(by='Month', ascending=True)
+print(arrival_df.head(10))
 
-incoming_df = arrival_df.merge(date_df, left_on='RWT', right_on='PO', how='inner')
-
+'''
 with open("test_output.txt", 'w') as file:
-    for row in incoming_df.to_string(index=False):
+    for row in arrival_df.to_string(index=False):
         file.write(row)
+        '''
